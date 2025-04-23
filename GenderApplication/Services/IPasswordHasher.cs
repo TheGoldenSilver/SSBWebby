fb8f0e419feb85477834a10f3b8bf51b85404677
GoldenSilver <snpgonzales7@gmail.com>
2025-04-23 19:24:33 +0800

    Revert "New set of code, improving its styles and functionality"
    This reverts commit e2b2eb485e2545387ed71f1388f9f35c0fb2adc3.

--- GenderApplication/Services/IPasswordHasher.cs
+++
@@ -1,8 +0,0 @@
-namespace GenderApplication.Services
-{
-    public interface IPasswordHasher
-    {
-        string HashPassword(string password);
-        bool VerifyPassword(string password, string hashedPassword);
-    }
-}
